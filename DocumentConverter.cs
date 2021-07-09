c474e5e0da7420d398fe66877f8a63279a8b4d4f
Radim Jilek <RJilek@moravia.com>
2021-07-09 08:32:01 +0200

    Input file validation, testing data

---
+++ DocumentConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.CodeDom;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace SummerTask
+{
+    public class DocumentConverter
+    {
+        public void Execute(string sourceFileName, string targetFileName)
+        {
+            if (!FileValidation(sourceFileName))
+            {
+                throw new FileNotFoundException($"Input file '{sourceFileName}' not found.");
+            }
+
+            var settings = new Settings()
+            {
+                InputType = GetFileType(sourceFileName)
+            };
+            string input;
+
+            try
+            {
+                FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
+                var reader = new StreamReader(sourceStream);
+                input = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            var xdoc = XDocument.Parse(input);
+            var doc = new Document
+            {
+                Title = xdoc.Root.Element("Title").Value,
+                Text = xdoc.Root.Element("Text").Value
+            };
+
+            var serializedDoc = JsonConvert.SerializeObject(doc);
+
+            File.WriteAllText(targetFileName, serializedDoc);
+        }
+
+        private InputType GetFileType(string sourceFileName)
+        {
+            var extension = Path.GetExtension(sourceFileName);
+            var clearExtension = extension.Replace(".", "");
+
+            if (Enum.TryParse(clearExtension, true, out InputType inputType))
+            {
+                return inputType;
+            }
+
+            throw new NotSupportedException($"Unsupported extension '{clearExtension}'");
+        }
+
+        private bool FileValidation(string fileName)
+        {
+            return File.Exists(fileName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace SummerTask
 {
@@ -14,12 +13,12 @@ namespace SummerTask
         private static void Main(string[] args)
         {
             // settings
-            // nazvy souboru jako parametry - cesta z parametru
             // validace souboru - existence souboru, validace obsahu
             // tridy na praci se souborem - FileOpener => FileParser => FileWriter
             // strategie na nacitani z disku, http
             // Reader/Writer - provider
             // factory na de/serializaci
+            // nazvy souboru jako parametry - cesta z parametru
             // pridat logger
             // testy -  prubezne
             // pridat kontejnery
@@ -27,34 +26,14 @@ namespace SummerTask
 
             //./app source output-target*
 
-            var sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
-            var targetFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Target Files\\Document1.json");
+            var sourceFileName = Path.Combine(Environment.CurrentDirectory, "Document1.xml");
+            var targetFileName = Path.Combine(Environment.CurrentDirectory, "Output1.json");
 
-            string input;
+            new DocumentConverter().Execute(sourceFileName, targetFileName);
 
-            try
-            {
-                FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
-                var reader = new StreamReader(sourceStream);
-                input = reader.ReadToEnd();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-
-            var xdoc = XDocument.Parse(input);
-            var doc = new Document
-            {
-                Title = xdoc.Root.Element("title").Value,
-                Text = xdoc.Root.Element("text").Value
-            };
-
-            var serializedDoc = JsonConvert.SerializeObject(doc);
-
-            var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
-            var sw = new StreamWriter(targetStream);
-            sw.Write(serializedDoc);
+            //var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
+            //var sw = new StreamWriter(targetStream);
+            //sw.Write(serializedDoc);
         }
     }
 }
